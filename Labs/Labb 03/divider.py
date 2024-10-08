79d9bb740c98617d8b5008a4d19a7ed6400df37e
AlexanderTarler <alexander.tarler@gmail.com>
2024-10-08 17:01:56 +0200

    Added 3 mathematical functions to substitute the default line in the graph, each one being shown in their own graph. I also added code that classifies each datapoint depending on each new mathematical function.

--- Labs/Labb 03/divider.py
+++ Labs/Labb 03/divider.py
@@ -12,42 +12,81 @@ x_values = []
 y_values = []
 data = []
 
-
-
-def classify_datapoint(x, y):
+# y = x
+def classify_datapoint_y_eq_x(x, y):
     return 1 if y > x else 0
+
+# f(x) = -0.489x
+def classify_datapoint_f(x, y):
+    return 1 if y > -0.489 * x else 0
+
+# g(x) = -2x + 0.16
+def classify_datapoint_g(x, y):
+    return 1 if y > -2 * x + 0.16 else 0
+
+# h(x) = 800x - 120
+def classify_datapoint_h(x, y):
+    return 1 if y > 800 * x - 120 else 0
 
 
 with open(input_file) as csv_file:
-        # The delimiter variable is used to separate the values in the csv-file using the commas as a reference
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            x, y = float(row[0]), float(row[1])
-            label = classify_datapoint(x, y)
-            x_values.append(x)
-            y_values.append(y)
-            data.append([x, y, label])
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        x, y = float(row[0]), float(row[1])
 
+        label_y_eq_x = classify_datapoint_y_eq_x(x, y)
+        label_f = classify_datapoint_f(x, y)
+        label_g = classify_datapoint_g(x, y)
+        label_h = classify_datapoint_h(x, y)
+        
+        x_values.append(x)
+        y_values.append(y)
 
-with open(output_file, 'w', newline='') as f_write:
-    csv_writer = csv.writer(f_write)
+        data.append([x, y, label_y_eq_x, label_f, label_g, label_h])
 
-    for row in data:
-        csv_writer.writerow(row)
-
-for i in range(len(data)):
-    if data[i][2] == 1:
-        plt.scatter(x_values[i], y_values[i], color='blue', label='Ovanför' if i == 0 else "")
-    else:
-        plt.scatter(x_values[i], y_values[i], color='red', label='Nedanför' if i == 0 else "")
 
 min_x = min(x_values)
 max_x = max(x_values)
 
-plt.plot([min_x, max_x], [min_x, max_x], color='black', label='y = x')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Klassificering av punkter med linjen y = x')
-plt.legend()
-plt.show()
 
+with open(output_file, 'w', newline='') as f_write:
+    csv_writer = csv.writer(f_write)
+    csv_writer.writerow(['x', 'y', 'label_y_eq_x', 'label_f', 'label_g', 'label_h'])
+    for row in data:
+        csv_writer.writerow(row)
+
+
+def plot_a_line(x_values, y_values, data, formula, line_label,  classification_index):
+
+    for i in range(len(data)):
+        if data[i][classification_index] == 1:
+            plt.scatter(x_values[i], y_values[i], color='blue', label='Ovanför' if i == 0 else "")
+        else:
+            plt.scatter(x_values[i], y_values[i], color='red', label='Nedanför' if i == 0 else "")
+
+    y_min = formula(min_x)
+    y_max = formula(max_x)
+
+
+    plt.plot([min_x, max_x], [y_min, y_max], color="black", label=line_label)
+
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(f'Klassificering av punkter med linjen {line_label}')
+    plt.legend()
+    plt.show()
+
+
+
+# y = x 
+plot_a_line(x_values, y_values, data, lambda x: x, 'y = x', 2)
+
+# f(x) = -0.489x 
+plot_a_line(x_values, y_values, data, lambda x: -0.489 * x, 'f(x) = -0.489x', 3)
+
+# g(x) = -2x + 0.16 
+plot_a_line(x_values, y_values, data, lambda x: -2 * x + 0.16, 'g(x) = -2x + 0.16', 4)
+
+# h(x) = 800x - 120 
+plot_a_line(x_values, y_values, data, lambda x: 800 * x - 120, 'h(x) = 800x - 120', 5)
